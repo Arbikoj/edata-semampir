b86958dae123ca83ae4f137e4ebdb43a6c29e5f9
Arbikoj <kojosan15@gmail.com>
2024-01-23 18:39:30 +0700

    super admin seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'Arbi',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'super-admin',
+        ]);
     }
 }
